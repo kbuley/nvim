97e39ee4a6060c9b36d7873c3b0f1dd09cb1ca3c
Kevin Buley <kevin@buley.org>
2024-03-28 15:02:59 -0400

    modify conform settings

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,6 +1,6 @@
 return {
   { -- Autoformat
-    'stevearc/conform.nvim',
+    "stevearc/conform.nvim",
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
@@ -14,13 +14,13 @@ return {
         }
       end,
       formatters_by_ft = {
-        lua = { 'stylua' },
+        lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { "isort", "black" },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { "prettierd", "prettier" } },
       },
     },
   },
