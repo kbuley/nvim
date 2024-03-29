6d10e6d9f89599274e5d6c741a049049676e6751
Kevin Buley <kevin@buley.org>
2024-03-29 14:04:33 -0400

    reformat tmux plugin

--- lua/custom/plugins/tmux.lua
+++ lua/custom/plugins/tmux.lua
@@ -1,7 +1,6 @@
 return {
-    "aserowy/tmux.nvim",	
-    config = function()
-	    require("tmux").setup() 
-    end
-    }
-
+  "aserowy/tmux.nvim",
+  config = function()
+    require("tmux").setup()
+  end,
+}
