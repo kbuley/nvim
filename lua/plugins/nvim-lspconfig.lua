b9313063c64e27c6c6b6801c4cb6fd740df15cce
Kevin Buley <kevin@buley.org>
2024-07-29 21:08:52 -0400

    disable ps1 lsp

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -7,7 +7,7 @@ return {
       yamlls = {},
       cssls = {},
       puppet = { cmd = { "puppet-languageserver", "--stdio" } },
-      ps1 = { "powershell_es" },
+      --      ps1 = { "powershell_es" },
       sourcery = {
         init_options = {
           token = "AAAAA",
