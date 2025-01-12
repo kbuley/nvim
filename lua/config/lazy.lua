89cefee1f54dad1152755a82646711422667cbab
Kevin Buley <kevin@buley.org>
2025-01-12 12:04:58 -0500

    add yanky and neogen

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -34,6 +34,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
 
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.coding.neogen" },
 
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
