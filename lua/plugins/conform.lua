217b5bf4307eee03a185abe5ead2d14d14516cb9
Kevin Buley <kevin@buley.org>
2025-07-14 19:24:49 -0400

    resolve more merge issues from develop

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,4 +1,5 @@
 return {
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
@@ -27,30 +28,5 @@ return {
         ["_"] = { "trim_whitespace" },
       },
     },
-    -- Set this to change the default values when calling conform.format()
-    -- This will also affect the default values for format_on_save/format_after_save
-    default_format_opts = {
-      lsp_format = "fallback",
-    },
-    -- If this is set, Conform will run the formatter on save.
-    -- It will pass the table to conform.format().
-    -- This can also be a function that returns the table.
-    -- format_on_save = {
-    --   -- I recommend these options. See :help conform.format for details.
-    --   lsp_format = "fallback",
-    --   timeout_ms = 500,
-    -- },
-    -- -- If this is set, Conform will run the formatter asynchronously after save.
-    -- -- It will pass the table to conform.format().
-    -- -- This can also be a function that returns the table.
-    -- format_after_save = {
-    --   lsp_format = "fallback",
-    -- },
-    -- Set the log level. Use `:ConformInfo` to see the location of the log file.
-    log_level = vim.log.levels.ERROR,
-    -- Conform will notify you when a formatter errors
-    notify_on_error = true,
-    -- Conform will notify you when no formatters are available for the buffer
-    notify_no_formatters = true,
   },
 }
