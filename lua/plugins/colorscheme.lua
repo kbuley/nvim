5bfab7944b297755c1841dc773a4ab8293b1f3ef
Kevin Buley <kevin@buley.org>
2025-09-01 12:14:01 -0400

    workaround for bufferline breaking change

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,17 @@
 return {
   "LazyVim/LazyVim",
+  requires = { "catppuccin/nvim" },
   opts = {
     colorscheme = "catppuccin-mocha",
+  },
+  -- Remove this after the PR is fixed: https://github.com/LazyVim/LazyVim/pull/6354
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
   },
 }
